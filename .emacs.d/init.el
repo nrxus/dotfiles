12ee1725c0025e7784e5b46ecca0aaf87eab42ce
Andrés Medina <andresnrx@gmail.com>
2020-07-21 20:56:26 -0700

    multiple cursors + expand region

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -143,8 +143,11 @@
   (setq undo-tree-visualizer-diff t)
   :diminish)
 
-;; expand selection
-(use-package expand-region)
+;; expand selection ala IDEA
+(use-package expand-region
+  :bind
+  ("<M-up>" . er/expand-region)
+  ("<M-down>" . er/contract-region))
 
 ;; useful commands
 (use-package crux
@@ -202,6 +205,36 @@ _SPC_ cancel
   )
 
 (key-chord-define-global "hh" 'hydra-hs/body)
+
+;; multi-cursor
+(use-package multiple-cursors)
+
+(defhydra hydra-multiple-cursors (:hint nil)
+  "
+ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cursor%s(if (> (mc/num-cursors) 1) \"s\" \"\")
+------------------------------------------------------------------
+ [_p_]   Next     [_n_]   Next     [_l_] Edit lines  [_0_] Insert numbers
+ [_P_]   Skip     [_N_]   Skip     [_a_] Mark all    [_A_] Insert letters
+ [_M-p_] Unmark   [_M-n_] Unmark   [_s_] Search
+ [Click] Cursor at point       [_q_] Quit"
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("s" mc/mark-all-in-region-regexp :exit t)
+  ("0" mc/insert-numbers :exit t)
+  ("A" mc/insert-letters :exit t)
+  ("<mouse-1>" mc/add-cursor-on-click)
+  ;; Help with click recognition in this hydra
+  ("<down-mouse-1>" ignore)
+  ("<drag-mouse-1>" ignore)
+  ("q" nil))
+
+(key-chord-define-global "mc" 'hydra-multiple-cursors/body)
 
 ;; -------------------------------- generic programming --------------------------------
 
